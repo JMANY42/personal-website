c580b0589a7623eebb2a60c49a0f76ddd1297cbf
JMANY42 <2187.j.man@gmail.com>
2026-01-14 15:28:48 -0600

    updated colors

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -10,11 +10,11 @@ function Navbar() {
         {text: 'Projects', path: '/projects'}
     ];
     return (
-        <div className="flex gap-10 px-8 py-4 bg-[#70E7FF]">
+        <div className="flex gap-10 px-8 py-4 bg-[#3D5A80]">
             {options.map((option, id) => (    // Map over options to create list items
                 <li 
                     key={id}
-                    className="cursor-pointer text-black text-2xl"> {/* List items styling */}
+                    className="cursor-pointer text-white text-2xl"> {/* List items styling */}
                     <NavbarOption text={option.text} path={option.path}></NavbarOption>
                 </li>
             ))}
