bbfc2fdbd0eee5e99ee11095bcf1d5cb70a8f84d
JMANY42 <2187.j.man@gmail.com>
2026-01-14 00:37:10 -0600

    about me v0

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -7,9 +7,9 @@ interface NavbarOption {
 function NavbarOption ({ img_path, caption }: NavbarOption) {
     
     return (
-        <div className="m-4">
-            <img className=""src={img_path}/>
-            <p className="text-white text-xl">{caption}</p>
+        <div className="m-8 p-4 space-y-4 rounded-2xl bg-[#FFD699]">
+            <img className="aspect-square w-full rounded-2xl"src={img_path}/>
+            <p className="text-black text-xl">{caption}</p>
         </div>
     )
 }
