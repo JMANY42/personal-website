0a3ede84cec2f472b57f1be5932a0a20e047b4cb
JMANY42 <2187.j.man@gmail.com>
2026-02-03 18:08:38 -0600

    correct slide path

--- frontend/src/api/slides.ts
+++ frontend/src/api/slides.ts
@@ -2,7 +2,7 @@ import type { SlideResponse } from '../types/slide.ts'
 
 export async function fetchSlides(): Promise<SlideResponse[]> {
   // const res = await fetch("http://localhost:5000/api/slides");    //LOCAL DEVELOPMENT ONLY
-  const res = await fetch("/api/slide");                      //SERVER ONLY
+  const res = await fetch("/api/slides");                      //SERVER ONLY
   if (!res.ok) throw new Error("Failed to fetch slides");     
   return res.json();
 }
