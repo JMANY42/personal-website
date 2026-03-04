447e8ee9b0853c046ebf106cd59425dbd5463118
JMANY42 <2187.j.man@gmail.com>
2026-03-04 16:08:35 -0600

    fix: remove duplicate test projects

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -43,7 +43,9 @@ function Home() {
 
   return (
     <div className="min-h-screen w-full bg-bg flex flex-col">
-      <Navbar />
+      <div className="sticky top-0 z-10 bg-bg">
+        <Navbar />
+      </div>
       
       <main className="flex-1 w-full page-padding py-6 sm:py-8 md:py-12 overflow-y-auto">
         <div className="w-full h-full">
