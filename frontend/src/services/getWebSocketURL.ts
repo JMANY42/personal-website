f855b1cef990ecfbb8944926e018d9cfa950ae16
JMANY42 <2187.j.man@gmail.com>
2026-03-06 00:48:56 -0600

    fix(canvas): correct websocket url for all three servers

--- frontend/src/services/getWebSocketURL.ts
+++ frontend/src/services/getWebSocketURL.ts
@@ -1,7 +1,7 @@
 export function getWebSocketURL() {
     // const production_port = window.location.port + 2000 // 3000 -> 5000, 3001 -> 5001
     return import.meta.env.MODE === 'production'
-    ? `wss://${window.location.hostname}:5000`
+    ? `ws://${window.location.hostname}/api/`
     : `ws://${window.location.hostname}:5000`;
 }
 
