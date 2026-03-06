083435ad057d13d5b5b8e72f4086b6b949a9e5a4
JMANY42 <2187.j.man@gmail.com>
2026-03-06 00:39:21 -0600

    fix(canvas): testing hardcoded websocket port for dev server

--- frontend/src/services/getWebSocketURL.ts
+++ frontend/src/services/getWebSocketURL.ts
@@ -1,7 +1,7 @@
 export function getWebSocketURL() {
-    const production_port = window.location.port + 2000 // 3000 -> 5000, 3001 -> 5001
+    // const production_port = window.location.port + 2000 // 3000 -> 5000, 3001 -> 5001
     return import.meta.env.MODE === 'production'
-    ? `wss://${window.location.hostname}:${production_port}`
+    ? `wss://${window.location.hostname}:5000`
     : `ws://${window.location.hostname}:5000`;
 }
 
