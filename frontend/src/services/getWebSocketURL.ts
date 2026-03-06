e1c2cc00ef3eeb6480bc2707c4ef5ea6af8720ac
JMANY42 <2187.j.man@gmail.com>
2026-03-06 00:53:09 -0600

    fix(canvas): wss websocket

--- frontend/src/services/getWebSocketURL.ts
+++ frontend/src/services/getWebSocketURL.ts
@@ -1,7 +1,7 @@
 export function getWebSocketURL() {
     // const production_port = window.location.port + 2000 // 3000 -> 5000, 3001 -> 5001
     return import.meta.env.MODE === 'production'
-    ? `ws://${window.location.hostname}/api/`
+    ? `wss://${window.location.hostname}/api/`
     : `ws://${window.location.hostname}:5000`;
 }
 
